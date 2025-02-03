3cbca083d0f5b9c765bcfadfb8b0b698405f173a
alimsarker <alimsarker@gmail.com>
2025-02-03 12:10:47 +0600

    first commit

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+       /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,2 @@
+<img src="{{ asset('logo/logo.png') }}" width="100px;">
+
