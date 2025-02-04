ef7265555fba59f06a249797fa652efbb9d94805
alimsarker <alimsarker@gmail.com>
2025-02-04 12:06:10 +0600

    4 Commit

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -8,7 +8,7 @@
     <div id="sidebar-menu">
         <!-- Left Menu Start -->
         <ul class="metismenu list-unstyled" id="side-menu">
-            <li class="menu-title">Menu</li>যযযযযযযযযয
+            <li class="menu-title">Menu</li>
 
             <li>
                 <a href="index.html" class="waves-effect">
